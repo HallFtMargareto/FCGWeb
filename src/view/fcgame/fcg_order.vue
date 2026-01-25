858e2ab6ea3f15acaf53ec31c0c586f86f4225ab
xusc <xusc@outlook.com>
2026-01-25 16:33:46 +0800

    style(fcg_order): 修复模板中插值表达式的缩进和对齐
    - 统一模板中插值表达式 `{{ ... }}` 的缩进，提升代码可读性
    - 调整“发单时间”与“创建时间”字段的显示顺序，使信息展示更符合逻辑

--- src/view/fcgame/fcg_order.vue
+++ src/view/fcgame/fcg_order.vue
@@ -153,7 +153,7 @@
         <el-col :span="6" v-if="statusTabState === '1' || mark_state === '1'">
           <el-button @click="toggleSelectAll">{{
             isAllSelected ? "取消全选" : "全选"
-            }}</el-button>
+          }}</el-button>
 
           <el-button v-if="statusTabState === '1'" icon="el-icon-s-unfold" @click="openBatchEditDialog">批量编辑</el-button>
           <el-button v-if="statusTabState === '1' && userInfo.perm['host']" icon="el-icon-delete"
@@ -220,7 +220,7 @@
             <el-descriptions :column="2" size="mini" border :labelStyle="{ width: '100px' }">
               <el-descriptions-item label="ID">{{
                 orderGroup.ID
-                }}</el-descriptions-item>
+              }}</el-descriptions-item>
 
               <el-descriptions-item label="总金额">
                 <span :style="{
@@ -240,7 +240,7 @@
 
               <el-descriptions-item label="识别次数">{{
                 orderGroup.version
-                }}</el-descriptions-item>
+              }}</el-descriptions-item>
               <el-descriptions-item label="代理佣金">¥ {{ orderGroup.commission }}</el-descriptions-item>
               <el-descriptions-item label="识别难度">
                 {{ getRiskLevelText(orderGroup.risk_score) }}
@@ -250,7 +250,7 @@
               </el-descriptions-item>
               <el-descriptions-item label="识别耗时">{{
                 orderGroup.message ? orderGroup.message.llmcons_at : ""
-                }}</el-descriptions-item>
+              }}</el-descriptions-item>
               <el-descriptions-item label="投注数量">
                 {{ orderGroup.total_bet_count }}
               </el-descriptions-item>
@@ -264,22 +264,18 @@
                   ¥ {{ orderGroup.win_amount }}
                 </span>
               </el-descriptions-item>
-              <el-descriptions-item label="创建时间">{{
-                orderGroup.created_at
-                }}</el-descriptions-item>
+              <el-descriptions-item label="发单时间">{{
+                $utils.formatTimeToStr(orderGroup.sort_seq)
+              }}</el-descriptions-item>
               <el-descriptions-item label="所属组织">
                 {{ getTenantName(orderGroup.tenant_id) }}
               </el-descriptions-item>
-              <!-- 
-sort_seq
-: 
-1768992256435 -->
-              <el-descriptions-item label="发单时间">{{
-                $utils.formatTimeToStr(orderGroup.sort_seq)
-                }}</el-descriptions-item>
+              <el-descriptions-item label="创建时间">{{
+                orderGroup.created_at
+              }}</el-descriptions-item>
               <el-descriptions-item label="单号">{{
                 orderGroup.order_no
-                }}</el-descriptions-item>
+              }}</el-descriptions-item>
             </el-descriptions>
           </div>
 
